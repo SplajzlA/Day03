111e55676f4c9a6b08c9d0ec3eca8ac0ff35ea43
SplaY <mirza_messic@hotmail.com>
2021-03-10 22:26:45 +0100

    Added sixth index.js

--- ex05/src/index.js
+++ ex05/src/index.js
@@ -1,12 +1,10 @@
 function main (){
 
-   var fooBoo = 0;
-   var x = 8
+   var x = 8;
    return x;
+
     }
-    
-    main (
-    
-    )
+
     console.log (main());
+
     module.exports = main;
