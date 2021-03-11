08a998d79ec31493b5be8ba40b24685dffa0f8be
SplaY <mirza_messic@hotmail.com>
2021-03-11 10:16:24 +0100

    Space changed

--- ex06/src/index.js
+++ ex06/src/index.js
@@ -2,11 +2,11 @@ function main (){
 // Only change code below this line
     var x = 7;
     var y = 12;
-    var z = "My name is ";
+    var z = "My name is";
     
     x = x + 2
     y = y + 4
-    z = z + "John!";
+    z = z + " John!";
 
     return [x, y, z]
  // Only change code above this line
